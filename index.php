f4a887e624e4d03190c589be6f659618d7c76bbc
alunos <alunos@172.17.10.159>
2016-10-11 09:59:37 -0300

    Arquivos de base do sistema criados.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Legendas TV</title>
+        <link rel="icon" href="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Revistas, compras, Leitura, Ler">
+        <meta name="description" content="Site para venda de revistas">
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body>
+        <table width="80%" align="center">
+            <tr>
+                <td>
+                    <div id="topo">
+                        <div id="titulo">
+                            LOJA ONLINE  <br>
+                            DE REVISTAS
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div></div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div></div>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
